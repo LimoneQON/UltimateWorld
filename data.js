94d2a420ac3f8f0b85d8574187d999403e370d9f
LimoneQON <105449293+LimoneQON@users.noreply.github.com>
2026-06-19 21:09:05 +0200

    Update data.js

--- data.js
+++ data.js
@@ -9,11 +9,25 @@ export const ITEM_DB = {
     'bomb': { sym: '💣', name: 'Bomba', use: false }
 };
 
-export const WEAPONS = [ {name: "Pięści", sym: "👊", dmg: 5}, {name: "Miecz", sym: "🗡️", dmg: 15}, {name: "Topór", sym: "🪓", dmg: 35}, {name: "Kij Maga", sym: "🪄", dmg: 60} ];
+export const HEROES = [
+    {name: "Michał Kutas", cls: "Wojownik", hp: 150, dmg: 15},
+    {name: "Dżon Pinat", cls: "Mag", hp: 60, dmg: 35},
+    {name: "Gibki Gibek", cls: "Łotr", hp: 90, dmg: 25},
+    {name: "Paskalito", cls: "Łucznik", hp: 80, dmg: 30}
+];
+
+export const WEAPONS = [ {name: "Pięści", sym: "👊", dmg: 0}, {name: "Miecz", sym: "🗡️", dmg: 15}, {name: "Topór", sym: "🪓", dmg: 35}, {name: "Kij Maga", sym: "🪄", dmg: 60} ];
 export const ARMORS = [ {name: "Szmaty", def: 0}, {name: "Zbroja", def: 15}, {name: "Mithril", def: 40} ];
 export const BELTS = [ {name: "Brak", cap: 5}, {name: "Pas", cap: 15}, {name: "Pas Tytanów", cap: 50} ];
 
 export const MOBS = [ {n: "Wilk", s: "🐺", hp: 30, d: 8}, {n: "Goblin", s: "👺", hp: 40, d: 12}, {n: "Demon", s: "👿", hp: 100, d: 25} ];
+
+export const NPC_POOL = [
+    {n: "Piotr 'Ciepły'", s: "🧔", text: "Uważaj! Rzymianie-Żydzi to tylko uśpieni Administratorzy symulacji! Znajdź kod źródłowy na dnie!"},
+    {n: "Gotka Oliwia 'Tom'", s: "🧛‍♀️", text: "Myślisz, że stąd uciekniesz? Homunkulusy z laboratoriów to błędy systemu zżerające pamięć RAM."},
+    {n: "Bułka Zulczyk", s: "🍞", text: "Cześć podróżniku. Wiesz, że kiedy dojdziesz na dno, czeka nas Format Dysku? Zrób zapasy."},
+    {n: "Tajemniczy Błąd", s: "👁️", text: "Aby zatrzymać symulację, połącz magię z technologią... Zmiażdż Homunkulusa."}
+];
 
 export const RECIPES = [
     {name: "Skórzany Pas (Max Stack x15)", req: {wood: 5}, type: 'belt'},
